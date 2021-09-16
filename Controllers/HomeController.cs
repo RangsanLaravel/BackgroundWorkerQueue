7824112565afbc2b8ed93d6d3393b806d8118046
Rangsan Chipphang <rangsan.chi@bla.co.th>
2021-09-16 20:54:24 +0700

    first commit ad queue

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using BackgroundWorkerQueue.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundWorkerQueue.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BackgroundWorkerQueues _backgroundWorkerQueue;
+
+        public HomeController(ILogger<HomeController> logger, BackgroundWorkerQueues backgroundWorkerQueue)
+        {
+            _logger = logger;
+            _backgroundWorkerQueue = backgroundWorkerQueue;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            await CallSlowApi();
+            return View();
+        }
+        private async Task CallSlowApi()
+        {
+            _logger.LogInformation($"Starting at {DateTime.UtcNow.TimeOfDay}");
+            _backgroundWorkerQueue.QueueBackgroundWorkItem(async token =>
+            {
+                await Task.Delay(10000);
+                _logger.LogInformation($"Done at {DateTime.UtcNow.TimeOfDay}");
+            });
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    
+
+    
+}
---
+++ Models/LongRunningService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundWorkerQueue.Models
+{
+    public class LongRunningService : BackgroundService
+    {
+        private readonly BackgroundWorkerQueues queue;
+
+        public LongRunningService(BackgroundWorkerQueues queue)
+        {
+            this.queue = queue;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var workItem = await queue.DequeueAsync(stoppingToken);
+
+                await workItem(stoppingToken);
+            }
+        }
+    }
+}
